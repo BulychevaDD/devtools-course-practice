081b94ced90a9eeb680c06dde27aa573ecb9ffec
BulychevaDD <61518917+BulychevaDD@users.noreply.github.com>
2022-05-29 11:46:31 +0300

    req_changes

--- modules/bulicheva_darya_bil_interpol/src/bil_interpol.cpp
+++ modules/bulicheva_darya_bil_interpol/src/bil_interpol.cpp
@@ -14,19 +14,19 @@ bilen_interpol::bilen_interpol(std::vector<double> _arr) {
 }
 
 double bilen_interpol::interpolate() {
-    double r;
+    double res;
     double r1;
     double r2;
-    double x = 10;
+    double x0 = 10;
     double x1 = 20;
     double x2 = 30;
     double y2 = 30;
     double y1 = 20;
-    double y = 10;
-    r1 = arr[0] + (x1 - x) * ((arr[1] - arr[0]) / (x2 - x));
-    r2 = arr[2] + (x1 - x) * ((arr[3] - arr[2]) / (x2 - x));
-    r = r1 + (y1 - y2) * ((r2 - r1) / (y - y2));
-    return r;
+    double y0 = 10;
+    r1 = arr[0] + (x1 - x0) * ((arr[1] - arr[0]) / (x2 - x0));
+    r2 = arr[2] + (x1 - x0) * ((arr[3] - arr[2]) / (x2 - x0));
+    res = r1 + (y1 - y2) * ((r2 - r1) / (y0 - y2));
+    return res;
 }
 
 bilen_interpol::~bilen_interpol() {
